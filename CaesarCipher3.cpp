dbc4692a3386b252e6b7418d0b760c4dbc3d8ddf
kba87 <57541889+kba87@users.noreply.github.com>
2020-04-10 19:43:20 +0200

    Add files via upload

---
+++ CaesarCipher3.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string.h>
+
+
+using namespace std;
+
+int main() {
+   cout<<"Enter the message:\n";
+   char msg[100];
+
+   cin.getline(msg,100);
+
+   int i, j, length,choice,key;
+
+
+   cout << "Enter Caesar key [1-25]: "<<endl;
+   cin >> key;
+
+   length = strlen(msg);
+    cout<<"Enter your choice: "<<endl;
+    cout<<" 1 Encryption "<<endl;
+    cout<<" 2 Decryption "<<endl;
+
+   cin>>choice;
+   if (choice==1) //for encryption{
+
+    {
+      char ch;
+      for(int i = 0; msg[i] != '\0'; ++i) {
+        char ch = msg[i];
+         //encrypt for lowercase letter
+         if (ch >= 'a' && ch <= 'z'){
+           char ch = ch + key;
+            if (ch > 'z') {
+               ch = ch - 'z' + 'a' - 1;
+            }
+            msg[i] = ch;
+         }
+         //encrypt for uppercase letter
+         else if (ch >= 'A' && ch <= 'Z'){
+            ch = ch + key;
+            if (ch > 'Z'){
+               ch = ch - 'Z' + 'A' - 1;
+            }
+            msg[i] = ch;
+         }
+      }
+      cout<<("Encrypted message: %s", msg);
+
+}   else if (choice == 2) { //for decryption
+      char ch;
+      for(int i = 0; msg[i] != '\0'; ++i) {
+         ch = msg[i];
+         //decrypt for lowercase letter
+         if(ch >= 'a' && ch <= 'z') {
+            ch = ch - key;
+            if(ch < 'a'){
+               ch = ch + 'z' - 'a' + 1;
+            }
+            msg[i] = ch;
+         }
+         //decrypt for uppercase letter
+         else if(ch >= 'A' && ch <= 'Z') {
+            ch = ch - key;
+            if(ch < 'A') {
+               ch = ch + 'Z' - 'A' + 1;
+            }
+            msg[i] = ch;
+         }
+      }
+      cout << "Decrypted message: " << msg;
+   }
+}
